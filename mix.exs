759a7937238b1fce1219cc66c4e068acd5a920e7
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-06-28 01:35:06 -0700

    Release v0.0.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wrap.MixProject do
   def project do
     [
       app: :wrap,
-      version: "0.0.5",
+      version: "0.0.6",
       elixir: "~> 1.9",
       description: description(),
       package: package(),
